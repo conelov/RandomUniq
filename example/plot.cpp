8c4d78beeb53c7662a61eec4d4b27bc1f1969fca
conelov <conelove@yandex.com>
2022-06-30 02:33:47 +0500

    plot form generation method union

--- example/plot.cpp
+++ example/plot.cpp
@@ -29,74 +29,62 @@ public:
   }
 
 private:
-  const std::map<QString, void (Plot::*)()> stringToMethod_;
+  const std::map<QString, QMap<double, double> (Plot::*)()> stringToMethod_;
 
 private:
   template<urand::UniformIntDistributionUniqGenType GenType>
-  void uniformIntDistributionUniqAtType() {
-    constexpr std::size_t              resolutionX = 100;
-    std::size_t                        resolutionY = 0;
-    std::map<std::size_t, std::size_t> data;
+  QMap<double, double> uniformIntDistributionUniqAtType() {
+    QMap<double, double>  data;
     {
       const std::size_t                                       repeatCount = sp_repeatCount->value();
       urand::UniformIntDistributionUniq<std::size_t, GenType> distribution(0, repeatCount);
       for ([[maybe_unused]] auto const i : ranges::views::iota(0u, repeatCount - 1u)) {
-        resolutionY = std::max(resolutionY, ++data[distribution()]);
+        ++data[distribution()];
       }
     }
-    auto const bars = new QCPBars(qcp_plot->xAxis, qcp_plot->yAxis);
-    bars->setWidth((1. / resolutionX) * 0.8);
-    for (auto const [key, value] : data) {
-      bars->addData(static_cast<double>(key) / resolutionX, static_cast<double>(value) / resolutionY);
-    }
+    return data;
   }
 
 
-  void method_UniformIntDistributionUniq_LinearDoobleGen() {
-    uniformIntDistributionUniqAtType<urand::UniformIntDistributionUniqGenType::LinearDoobleGen>();
+  QMap<double, double> method_UniformIntDistributionUniq_LinearDoobleGen() {
+    return uniformIntDistributionUniqAtType<urand::UniformIntDistributionUniqGenType::LinearDoobleGen>();
   }
 
 
-  void method_UniformIntDistributionUniq_NonLinearEqualChanceRange() {
-    uniformIntDistributionUniqAtType<urand::UniformIntDistributionUniqGenType::NonLinearEqualChanceRange>();
+  QMap<double, double> method_UniformIntDistributionUniq_NonLinearEqualChanceRange() {
+    return uniformIntDistributionUniqAtType<urand::UniformIntDistributionUniqGenType::NonLinearEqualChanceRange>();
   }
 
 
-  void linear_impl(std::size_t count) {
-    constexpr std::size_t              resolutionX = 100;
-    std::size_t                        resolutionY = 0;
-    std::map<std::size_t, std::size_t> data;
+  QMap<double, double> linear_impl(std::size_t count) {
+    QMap<double, double> data;
     {
       const std::size_t                          repeatCount = sp_repeatCount->value();
-      std::uniform_int_distribution<std::size_t> distribution(0, resolutionX);
+      std::uniform_int_distribution<std::size_t> distribution(0, repeatCount);
       for ([[maybe_unused]] auto const i : ranges::views::iota(0u, repeatCount - 1u)) {
         auto const key = ranges::accumulate(ranges::views::iota(0u, count), 0, [&](auto const lhs, auto const rhs) {
           return lhs + distribution(urand::util::RandomDevice<std::mt19937>::get());
         }) / count;
-        resolutionY    = std::max(resolutionY, ++data[key]);
+        ++data[key];
       }
     }
-    auto const bars = new QCPBars(qcp_plot->xAxis, qcp_plot->yAxis);
-    bars->setWidth((1. / resolutionX) * 0.9);
-    for (auto const [key, value] : data) {
-      bars->addData(static_cast<double>(key) / resolutionX, static_cast<double>(value) / resolutionY);
-    }
+    return data;
   }
 
 
-  void method_linear() {
-    linear_impl(1);
+  QMap<double, double> method_linear() {
+    return linear_impl(1);
   }
 
 
-  void method_linear_x3() {
-    linear_impl(3);
+  QMap<double, double> method_linear_x3() {
+    return linear_impl(3);
   }
 
 private slots:
   void on_pb_gen_released() {
     qcp_plot->clearPlottables();
-    std::invoke(stringToMethod_.at(cb_genMethod->currentText()), this);
+    auto const data = std::invoke(stringToMethod_.at(cb_genMethod->currentText()), this);
     qcp_plot->replot();
   }
 };
