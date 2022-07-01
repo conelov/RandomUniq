b979dfecc5d5eddd8ca5f7444540bb8f0e954b24
conelov <conelove@yandex.com>
2022-07-01 15:51:27 +0500

    rangeScaleView codestyle

--- example/util/rangeScaleView.hpp
+++ example/util/rangeScaleView.hpp
@@ -14,29 +14,20 @@
 
 namespace urand::plot::util {
 
-namespace aux {
-
-template<typename T>
-struct PairKeyDouble {
-  using type = std::pair<double, typename std::remove_cvref_t<decltype(std::declval<T>().second)>>;
-};
-}// namespace aux
-
-
 auto rangeScaleView(auto fromMin, auto fromMax, std::size_t count, auto toMin, auto toMax) {
   minmaxException(fromMin, fromMax);
   minmaxException(toMin, toMax);
 
   return ranges::views::transform(
            [count, scaler = RangeScaler(fromMin, fromMax, toMin, toMax * count)](auto pair) {
-             return typename aux::PairKeyDouble<decltype(pair)>::type{
+             return std::pair<double, typename std::remove_cvref_t<decltype(pair.second)>>{
                scaler(pair.first) / static_cast<double>(count), pair.second};
            })
     | ranges::views::chunk_by([](auto lhs, auto rhs) {
         return lhs.first == rhs.first;
       })
     | ranges::views::transform([](auto&& range) {
-        return typename aux::PairKeyDouble<decltype(ranges::front(range))>::type{
+        return decltype(ranges::front(range)){
           ranges::front(range).first, ranges::accumulate(std::forward<decltype(range)>(range), 0, [](auto lhs, auto pair) {
             return lhs + pair.second;
           })};
