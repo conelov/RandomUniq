744cebb7ea8243b96fcf500d07b02cadaab6e985
conelov <conelove@yandex.com>
2022-07-01 15:46:35 +0500

    rangeScaleView done

---
+++ example/util/rangeScaleView.hpp
@@ -0,0 +1,45 @@
+//
+// Created by dym on 01.07.22.
+//
+
+#pragma once
+
+#include "example/util/minmaxException.hpp"
+#include "example/util/RangeScaler.hpp"
+#include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/range/operations.hpp>
+#include <range/v3/view/chunk_by.hpp>
+#include <range/v3/view/transform.hpp>
+
+
+namespace urand::plot::util {
+
+namespace aux {
+
+template<typename T>
+struct PairKeyDouble {
+  using type = std::pair<double, typename std::remove_cvref_t<decltype(std::declval<T>().second)>>;
+};
+}// namespace aux
+
+
+auto rangeScaleView(auto fromMin, auto fromMax, std::size_t count, auto toMin, auto toMax) {
+  minmaxException(fromMin, fromMax);
+  minmaxException(toMin, toMax);
+
+  return ranges::views::transform(
+           [count, scaler = RangeScaler(fromMin, fromMax, toMin, toMax * count)](auto pair) {
+             return typename aux::PairKeyDouble<decltype(pair)>::type{
+               scaler(pair.first) / static_cast<double>(count), pair.second};
+           })
+    | ranges::views::chunk_by([](auto lhs, auto rhs) {
+        return lhs.first == rhs.first;
+      })
+    | ranges::views::transform([](auto&& range) {
+        return typename aux::PairKeyDouble<decltype(ranges::front(range))>::type{
+          ranges::front(range).first, ranges::accumulate(std::forward<decltype(range)>(range), 0, [](auto lhs, auto pair) {
+            return lhs + pair.second;
+          })};
+      });
+}
+}// namespace urand::plot::util
