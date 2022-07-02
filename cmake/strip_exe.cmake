4c9066e3641156445ee38b2bda33f7111f9af30b
conelov <conelove@yandex.com>
2022-07-03 03:10:06 +0500

    work cross-compiling and cmake add strip_exe.cmake

---
+++ cmake/strip_exe.cmake
@@ -0,0 +1,10 @@
+function(strip_target_binary TARGET)
+  if (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
+    add_custom_command(
+      TARGET ${TARGET}
+      POST_BUILD
+      COMMAND strip -v -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu. --remove-section=.note.ABI-tag --remove-section=.eh_frame --remove-section=.eh_frame_ptr $<TARGET_FILE:${TARGET}>
+      VERBATIM
+    )
+  endif ()
+endfunction()
